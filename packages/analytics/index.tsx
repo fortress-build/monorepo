6c514573a0ee19832a5185f9adc1943ee24b946a
Will Hopkins <willothyh@gmail.com>
2025-01-05 14:42:27 -0800

    refactor: delete feature flags package

--- packages/analytics/index.tsx
+++ packages/analytics/index.tsx
@@ -1,8 +1,7 @@
-import type { ReactNode } from 'react';
-import { GoogleAnalytics } from './google';
-import { keys } from './keys';
-import { PostHogProvider } from './posthog/client';
-import { VercelAnalytics } from './vercel';
+import type { ReactNode } from "react";
+import { GoogleAnalytics } from "./google";
+import { keys } from "./keys";
+import { PostHogProvider } from "./posthog/client";
 
 type AnalyticsProviderProps = {
   readonly children: ReactNode;
@@ -13,7 +12,6 @@ const { NEXT_PUBLIC_GA_MEASUREMENT_ID } = keys();
 export const AnalyticsProvider = ({ children }: AnalyticsProviderProps) => (
   <PostHogProvider>
     {children}
-    <VercelAnalytics />
     {NEXT_PUBLIC_GA_MEASUREMENT_ID && (
       <GoogleAnalytics gaId={NEXT_PUBLIC_GA_MEASUREMENT_ID} />
     )}
