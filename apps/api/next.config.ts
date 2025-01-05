4aa79d1cedfa7dd47ed351459a0e619faeb7eb17
Will Hopkins <willothyh@gmail.com>
2025-01-05 15:14:49 -0800

    refactor: remove more unused / vercel-dependent stuff

--- apps/api/next.config.ts
+++ apps/api/next.config.ts
@@ -1,15 +1,13 @@
-import { env } from '@/env';
-import { config, withAnalyzer } from '@repo/next-config';
-import { withLogtail, withSentry } from '@repo/observability/next-config';
-import type { NextConfig } from 'next';
+import { env } from "@/env";
+import { config, withAnalyzer } from "@repo/next-config";
+import { withLogtail, withSentry } from "@repo/observability/next-config";
+import type { NextConfig } from "next";
 
 let nextConfig: NextConfig = withLogtail({ ...config });
 
-if (env.VERCEL) {
-  nextConfig = withSentry(nextConfig);
-}
+nextConfig = withSentry(nextConfig);
 
-if (env.ANALYZE === 'true') {
+if (env.ANALYZE === "true") {
   nextConfig = withAnalyzer(nextConfig);
 }
 
