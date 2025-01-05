444254f27c5eab3c6aca54cf8df88bf3357fe622
Will Hopkins <willothyh@gmail.com>
2025-01-05 15:16:46 -0800

    refactor: small next config update

--- apps/api/next.config.ts
+++ apps/api/next.config.ts
@@ -3,9 +3,7 @@ import { config, withAnalyzer } from "@repo/next-config";
 import { withLogtail, withSentry } from "@repo/observability/next-config";
 import type { NextConfig } from "next";
 
-let nextConfig: NextConfig = withLogtail({ ...config });
-
-nextConfig = withSentry(nextConfig);
+let nextConfig: NextConfig = withSentry(withLogtail({ ...config }));
 
 if (env.ANALYZE === "true") {
   nextConfig = withAnalyzer(nextConfig);
