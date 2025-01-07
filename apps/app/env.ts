f16ac3bd14322e1f6337df5bc7ca0485dea2c48b
Will Hopkins <willothyh@gmail.com>
2025-01-06 17:35:40 -0800

    refactor: remove collaboration package and google analytics

--- apps/app/env.ts
+++ apps/app/env.ts
@@ -1,6 +1,5 @@
 import { keys as analytics } from "@repo/analytics/keys";
 import { keys as auth } from "@repo/auth/keys";
-import { keys as collaboration } from "@repo/collaboration/keys";
 import { keys as database } from "@repo/database/keys";
 import { keys as email } from "@repo/email/keys";
 import { keys as core } from "@repo/next-config/keys";
@@ -13,7 +12,6 @@ export const env = createEnv({
   extends: [
     auth(),
     analytics(),
-    collaboration(),
     core(),
     database(),
     email(),
--- packages/analytics/index.tsx
+++ packages/analytics/index.tsx
@@ -1,19 +1,10 @@
 import type { ReactNode } from "react";
-import { GoogleAnalytics } from "./google";
-import { keys } from "./keys";
 import { PostHogProvider } from "./posthog/client";
 
 type AnalyticsProviderProps = {
   readonly children: ReactNode;
 };
 
-const { NEXT_PUBLIC_GA_MEASUREMENT_ID } = keys();
-
 export const AnalyticsProvider = ({ children }: AnalyticsProviderProps) => (
-  <PostHogProvider>
-    {children}
-    {NEXT_PUBLIC_GA_MEASUREMENT_ID && (
-      <GoogleAnalytics gaId={NEXT_PUBLIC_GA_MEASUREMENT_ID} />
-    )}
-  </PostHogProvider>
+  <PostHogProvider>{children}</PostHogProvider>
 );
