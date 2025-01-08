c87bb0afbe8c43d7c6b6a70afcb3d18b968dfb7b
David Chu <david@fetchflow.ai>
2025-01-08 11:10:53 -0800

    followed suggested changes

--- apps/docs/components/Frame.tsx
+++ apps/docs/components/Frame.tsx
@@ -1,4 +1,3 @@
-// biome-ignore lint/correctness/noUndeclaredVariables: <explanation>
 export function Frame({ children }: { children: React.ReactNode }) {
   return (
     <div className='my-4 overflow-hidden rounded-lg border border-zinc-200 dark:border-zinc-700'>
