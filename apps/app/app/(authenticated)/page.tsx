60089e656724a3efb384d41dfe8b7ab7287e3e1d
Will Hopkins <willothyh@gmail.com>
2025-01-08 16:34:36 -0800

    remove template code from app

--- apps/app/app/(authenticated)/page.tsx
+++ apps/app/app/(authenticated)/page.tsx
@@ -1,5 +1,4 @@
 import { auth } from "@repo/auth/server";
-import { database } from "@repo/database";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { Header } from "./components/header";
@@ -13,7 +12,6 @@ export const metadata: Metadata = {
 };
 
 const App = async () => {
-  const pages = await database.page.findMany();
   const { orgId } = await auth();
 
   if (!orgId) {
@@ -25,11 +23,7 @@ const App = async () => {
       <Header pages={["Building Your Application"]} page="Data Fetching" />
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          {pages.map((page) => (
-            <div key={page.id} className="aspect-video rounded-xl bg-muted/50">
-              {page.name}
-            </div>
-          ))}
+          {`Welcome to ${orgId}'s Nerve dashboard!`}
         </div>
         <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
       </div>
