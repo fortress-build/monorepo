6fa002e496c9e6ff0390010dfd656f9277b7a6cd
Will Hopkins <willothyh@gmail.com>
2025-01-09 08:31:19 -0800

    chore: bump dependency versions (#5)

--- packages/design-system/components/ui/calendar.tsx
+++ packages/design-system/components/ui/calendar.tsx
@@ -60,8 +60,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+        IconLeft: ({ className, ...props }) => (
+          <ChevronLeftIcon className={cn("h-4 w-4", className)} style={props.style}  />
+        ),
+        IconRight: ({ className, ...props }) => (
+          <ChevronRightIcon className={cn("h-4 w-4", className)} style={props.style} />
+        ),
       }}
       {...props}
     />
