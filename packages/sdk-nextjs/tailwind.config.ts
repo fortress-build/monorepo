ef99ca6404803d1b12c68fa7d85a97f43be269c5
Will Hopkins <willothyh@gmail.com>
2025-01-12 13:27:54 -0800

    fix(nextjs): don't depend on private packages

--- packages/sdk-nextjs/tailwind.config.ts
+++ packages/sdk-nextjs/tailwind.config.ts
@@ -1,1 +1,12 @@
-export { config as default } from "@repo/tailwind-config/config";
+import type { Config } from "tailwindcss";
+
+export const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./node_modules/@nerve-js/next/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./providers/**/*.{ts,tsx}",
+  ],
+};
