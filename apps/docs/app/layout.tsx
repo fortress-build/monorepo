7b2cb7e664bbe04bc1784e0f89a5010010f0de4f
Will Hopkins <willothyh@gmail.com>
2025-01-24 21:45:31 -0800

    ci: deployment for docs

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -4,6 +4,8 @@ import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 
+//
+
 const inter = Inter({
   subsets: ["latin"],
 });
