d3b3118e2ff1a501ce58a26fb59f2b5dce923cb1
tomyang11 <tomyang1188@gmail.com>
2025-01-31 10:10:13 -0800

    added test pages on demo app

--- examples/next15/app/providers.tsx
+++ examples/next15/app/providers.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import type React from "react";
 import { NerveProvider } from "@nerve-js/next";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <NerveProvider
+    <NerveProvider
         redirectUrl="http://localhost:3000/callback/"
         signInUrl="http://localhost:3000/signin/"
         afterRedirectUrl="http://localhost:3000/"
-      >
-        {children}
-      </NerveProvider>
-    </>
+    >
+      {children}
+    </NerveProvider>
   );
 }
