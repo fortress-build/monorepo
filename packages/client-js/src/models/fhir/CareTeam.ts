27c21703045599ed3c1cd7c66d23b2a819182ab9
David Chu <david@fetchflow.ai>
2025-01-30 19:05:29 -0800

    test

--- packages/client-js/src/models/fhir/CareTeam.ts
+++ packages/client-js/src/models/fhir/CareTeam.ts
@@ -13,20 +13,23 @@ import type { Reference } from "./Reference";
 import type { RelatedPerson } from "./RelatedPerson";
 import { DomainResource } from "./Resource";
 
-// export const BodyStructure = v.object({
-//   resourceType: v.literal("BodyStructure"),
-//   identifier: v.optional(v.array(Identifier)),
-//   active: v.optional(v.boolean()),
-//   description: v.optional(v.string()),
-//   image: v.optional(v.array(Attachment)),
-//   patient: Reference<Patient>(),
-//
-//   includedStructure: v.array(BodyStructureLocation),
-//   excludedStructure: v.array(BodyStructureLocation),
-// });
 
+/**
+ * Identifies all people and organizations who are expected to be
+ * involved in the care team.
+ */
 export declare class CareTeamParticipant {
+  /**
+   * Indicates specific responsibility of an individual within the care
+   * team, such as &quot;Primary care physician&quot;, &quot;Trained social worker
+   * counselor&quot;, &quot;Caregiver&quot;, etc.
+   */
   role?: CodeableConcept[];
+
+  /**
+   * The specific person or organization who is participating/expected to
+   * participate in the care team.
+   */
   member?: Reference<
     | Practitioner
     | PractitionerRole
@@ -35,27 +38,96 @@ export declare class CareTeamParticipant {
     | Organization
     | CareTeam
   >;
+
+  /**
+   * The organization of the practitioner.
+   */
   onBehalfOf?: Reference<Organization>;
+
+  /**
+   * Indicates when the specific member or organization did (or is intended
+   * to) come into effect and end.
+   */
   period?: Period;
 }
 
+/**
+ * The Care Team includes all the people and organizations who plan to
+ * participate in the coordination and delivery of care for a patient.
+ */
 export declare class CareTeam extends DomainResource {
-  resourceType: "CareTeam";
+  resourceType: 'CareTeam';
+
+  /**
+   * Business identifiers assigned to this care team by the performer or
+   * other systems which remain constant as the resource is updated and
+   * propagates from server to server.
+   */
   identifier?: Identifier[];
+  /**
+   * Indicates the current state of the care team.
+   */
   status?:
-    | "proposed"
-    | "active"
-    | "suspended"
-    | "inactive"
-    | "entered-in-error";
+    | 'proposed'
+    | 'active'
+    | 'suspended'
+    | 'inactive'
+    | 'entered-in-error';
+  /**
+   * Identifies what kind of team.  This is to support differentiation
+   * between multiple co-existing teams, such as care plan team, episode of
+   * care team, longitudinal care team.
+   */
   category?: CodeableConcept[];
+
+  /**
+   * A label for human use intended to distinguish like teams.  E.g. the
+   * &quot;red&quot; vs. &quot;green&quot; trauma teams.
+   */
   name?: string;
+
+  /**
+   * Identifies the patient or group whose intended care is handled by the
+   * team.
+   */
   subject?: Reference<Patient | Group>;
+
+  /**
+   * Indicates when the team did (or is intended to) come into effect and
+   * end.
+   */
   period?: Period;
+
+  /**
+   * Identifies all people and organizations who are expected to be
+   * involved in the care team.
+   */
   participant?: CareTeamParticipant[];
+
+  /**
+   * Describes why the care team exists.
+   */
   reasonCode?: CodeableConcept[];
+
+  /**
+   * Condition(s) that this care team addresses.
+   */
   reasonReference?: Reference<Condition>[];
+
+  /**
+   * The organization responsible for the care team.
+   */
   managingOrganization?: Reference<Organization>[];
+
+  /**
+   * A central contact detail for the care team (that applies to all
+   * members).
+   */
   telecom?: ContactPoint[];
+
+  /**
+   * Comments made about the CareTeam.
+   */
   note?: Annotation[];
 }
+
--- packages/client-js/src/models/fhir/CatalogEntry.ts
+++ packages/client-js/src/models/fhir/CatalogEntry.ts
@@ -21,19 +21,90 @@ export interface CatalogEntryRelatedEntry {
     item: Reference<CatalogEntry>;
 }
 
-export declare class CatalogEntry extends DomainResource{
-    resourceType: 'CatalogEntry';
-    identifier?: Identifier[];
-    type?: CodeableConcept;
-    orderable: boolean;
-    referencedItem: Reference<Medication|Device| Organization| Practitioner | PractitionerRole | HealthcareService | ActivityDefinition | PlanDefinition | SpecimenDefinition | ObservationDefinition | Binary>;
-    additionalIdentifier?: Identifier[];
-    classification?: CodeableConcept[];
-    status?: 'draft' | 'active' | 'retired' | 'unknown';
-    validityPeriod?: Period;
-    validTo?: string;
-    lastUpdated?: string;
-    additionalCharacteristic?: CodeableConcept[];
-    additionalClassification?: CodeableConcept[];
-    relatedEntry?: CatalogEntryRelatedEntry[];
+/**
+ * Catalog entries are wrappers that contextualize items included in a
+ * catalog.
+ */
+export declare class CatalogEntry extends DomainResource {
+  resourceType: 'CatalogEntry';
+  /**
+   * Used in supporting different identifiers for the same product, e.g.
+   * manufacturer code and retailer code.
+   */
+  identifier?: Identifier[];
+
+  /**
+   * The type of item - medication, device, service, protocol or other.
+   */
+  type?: CodeableConcept;
+
+  /**
+   * Whether the entry represents an orderable item.
+   */
+  orderable: boolean;
+
+  /**
+   * The item in a catalog or definition.
+   */
+  referencedItem: Reference<
+    | Medication
+    | Device
+    | Organization
+    | Practitioner
+    | PractitionerRole
+    | HealthcareService
+    | ActivityDefinition
+    | PlanDefinition
+    | SpecimenDefinition
+    | ObservationDefinition
+    | Binary
+  >;
+
+  /**
+   * Used in supporting related concepts, e.g. NDC to RxNorm.
+   */
+  additionalIdentifier?: Identifier[];
+
+  /**
+   * Classes of devices, or ATC for medication.
+   */
+  classification?: CodeableConcept[];
+
+  /**
+   * Used to support catalog exchange even for unsupported products, e.g.
+   * getting list of medications even if not prescribable.
+   */
+  status?: 'draft' | 'active' | 'retired' | 'unknown';
+
+  /**
+   * The time period in which this catalog entry is expected to be active.
+   */
+  validityPeriod?: Period;
+
+  /**
+   * The date until which this catalog entry is expected to be active.
+   */
+  validTo?: string;
+
+  /**
+   * Typically date of issue is different from the beginning of the
+   * validity. This can be used to see when an item was last updated.
+   */
+  lastUpdated?: string;
+
+  /**
+   * Used for examplefor Out of Formulary, or any specifics.
+   */
+  additionalCharacteristic?: CodeableConcept[];
+
+  /**
+   * User for example for ATC classification, or.
+   */
+  additionalClassification?: CodeableConcept[];
+
+  /**
+   * Used for example, to point to a substance, or to a device used to
+   * administer a medication.
+   */
+  relatedEntry?: CatalogEntryRelatedEntry[];
 }
