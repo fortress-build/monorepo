0b989b69572a976a0911e3c973b718d601ba9c20
David Chu <david@fetchflow.ai>
2025-02-02 11:53:45 -0800

    maybe fixed some bundling problems

--- packages/client-js/vite.config.ts
+++ packages/client-js/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import path from "node:path";
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   plugins: [
@@ -14,13 +14,19 @@ export default defineConfig({
       // This will generate .d.ts files in the `dist` folder
       // and also make sure your package.json "types" point is correct.
       insertTypesEntry: true,
+      copyDtsFiles: true,
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      beforeWriteFile: (filePath, content) => ({
+        filePath,
+        content: content.replace(/\/\*\s*@internal\s*\*\//g, ''),
+      }),
     }),
   ],
   build: {
     lib: {
-      entry: "src/index.ts",
+      entry: 'src/index.ts',
       fileName: (format) => `index.${format}.js`,
-      formats: ["es", "cjs"],
+      formats: ['es', 'cjs'],
     },
   },
 });
