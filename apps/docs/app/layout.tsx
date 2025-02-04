bcd695376cab927c757e6a90ae24b9cfea63ffa1
David Chu <david@fetchflow.ai>
2025-02-04 13:41:35 -0800

    deploy docs

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -4,7 +4,7 @@ import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 
-//
+
 
 const inter = Inter({
   subsets: ["latin"],
